cdcdf608547db2575125108a5b299e02a6a966b9
Simi O <coolkid1984@ymail.com>
2023-12-11 12:10:56 -0600

    first test

---
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -0,0 +1,5 @@
+describe('ticketListReducer', () => {
+  test('Should return default state if there is no action type passed into the reducer', () => {
+    expect(ticketListReducer({}, { type: null })).toEqual({});
+  });
+});
