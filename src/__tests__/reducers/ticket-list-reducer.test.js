b7d78c3779ba7485b29bddc1d649626bac21c9fb
Simi O <coolkid1984@ymail.com>
2023-12-11 15:01:26 -0600

    fix changes to overall project in test area

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -4,24 +4,31 @@ describe('ticketListReducer', () => {
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(ticketListReducer({}, { type: null })).toEqual({});
   });
-});
 
-test('Should successfully add new ticket data to mainTicketList', () => {
-  const { names, location, issue, id } = ticketData;
-  action = {
-    type: 'ADD_TICKET',
-    names: names,
-    location: location,
-    issue: issue,
-    id: id
+  const ticketData = {
+    names: 'Ryan & Aimen',
+    location: '4b',
+    issue: 'Redux action is not working correctly.',
+    id: 1
   };
 
-  expect(ticketListReducer({}, action)).toEqual({
-    [id] : {
+  test('Should successfully add new ticket data to mainTicketList', () => {
+    const { names, location, issue, id } = ticketData;
+    const action = {
+      type: 'ADD_TICKET',
       names: names,
       location: location,
       issue: issue,
       id: id
-    }
+    };
+
+    expect(ticketListReducer({}, action)).toEqual({
+      [id] : {
+        names: names,
+        location: location,
+        issue: issue,
+        id: id
+      }
+    });
   });
 });
