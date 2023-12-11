56d962d46b7e9bc9e50bdb1fb278f701454ae41f
Simi O <coolkid1984@ymail.com>
2023-12-11 13:51:25 -0600

    add second test for adding tickets

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -1,5 +1,27 @@
+import ticketListReducer from '../../reducers/ticket-list-reducer';
+
 describe('ticketListReducer', () => {
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(ticketListReducer({}, { type: null })).toEqual({});
+  });
+});
+
+test('Should successfully add new ticket data to mainTicketList', () => {
+  const { names, location, issue, id } = ticketData;
+  action = {
+    type: 'ADD_TICKET',
+    names: names,
+    location: location,
+    issue: issue,
+    id: id
+  };
+
+  expect(ticketListReducer({}, action)).toEqual({
+    [id] : {
+      names: names,
+      location: location,
+      issue: issue,
+      id: id
+    }
   });
 });
