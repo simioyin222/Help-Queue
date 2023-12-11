ff40894eed2c516d1fa32eed066c01e3951baa9c
Simi O <coolkid1984@ymail.com>
2023-12-11 15:39:33 -0600

    add third redux test

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -32,3 +32,18 @@ describe('ticketListReducer', () => {
     });
   });
 });
+
+const currentState = {
+  1: { names: 'Ryan & Aimen', location: '4b', issue: 'Redux action is not working correctly.', id: 1 },
+  2: { names: 'Jasmine and Justine', location: '2a', issue: 'Reducer has side effects.', id: 2 }
+};
+
+test('Should successfully delete a ticket', () => {
+  const action = {
+    type: 'DELETE_TICKET',
+    id: 1
+  };
+  expect(ticketListReducer(currentState, action)).toEqual({
+    2: currentState[2]
+  });
+});
